ed065ca903ee35955ac870b18a1cd07734834e54
gabruw <gabrielmarques.mg@gmail.com>
2023-04-08 11:17:42 -0300

    :pencil2: Fixing Redux Persist mocks

--- src/__mocks__/setup/redux-persist.ts
+++ src/__mocks__/setup/redux-persist.ts
@@ -2,3 +2,5 @@ jest.mock("redux-persist", () => ({
     ...jest.requireActual("redux-persist"),
     persistReducer: jest.fn().mockImplementation((config, reducers) => reducers),
 }));
+
+export {};
